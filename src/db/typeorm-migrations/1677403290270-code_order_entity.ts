3743aa541cb9940c43230f1b5d4624cf19584084
kabdik <87753859226.ansar@gmail.com>
2023-02-26 15:23:30 +0600

    db: code order entity

--- src/db/typeorm-migrations/1677403290270-code_order_entity.ts
+++ src/db/typeorm-migrations/1677403290270-code_order_entity.ts
@@ -1,15 +1,17 @@
 import type { MigrationInterface, QueryRunner } from "typeorm";
 
-export class orderAddressCascade1677398133572 implements MigrationInterface {
-    name = 'orderAddressCascade1677398133572'
+export class codeOrderEntity1677403290270 implements MigrationInterface {
+    name = 'codeOrderEntity1677403290270'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "order_address" DROP CONSTRAINT "FK_d82a2840b5e5c98c569d75e92e8"`);
+        await queryRunner.query(`ALTER TABLE "order" ADD "code" character varying`);
         await queryRunner.query(`ALTER TABLE "order_address" ADD CONSTRAINT "FK_d82a2840b5e5c98c569d75e92e8" FOREIGN KEY ("orderId") REFERENCES "order"("id") ON DELETE CASCADE ON UPDATE CASCADE`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "order_address" DROP CONSTRAINT "FK_d82a2840b5e5c98c569d75e92e8"`);
+        await queryRunner.query(`ALTER TABLE "order" DROP COLUMN "code"`);
         await queryRunner.query(`ALTER TABLE "order_address" ADD CONSTRAINT "FK_d82a2840b5e5c98c569d75e92e8" FOREIGN KEY ("orderId") REFERENCES "order"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
     }
 
